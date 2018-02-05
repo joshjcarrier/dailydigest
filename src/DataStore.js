0f79a5510d204d833dc1b5b3b071b575614155b6
Oliver Zheng <gitcommits@oliverzheng.com>
2018-02-04 17:27:09 -0800

    Add groups and competitions stub data

--- src/DataStore.js
+++ src/DataStore.js
@@ -3,15 +3,50 @@ const DataStore = {
     userID: 1,
     username: 'joshcarrier',
     firstName: 'Josh',
+    groups: [2, 3, 5],
   }, {
     userID: 2,
     username: 'qijinzhou',
     firstName: 'Qijin',
+    groups: [2, 4, 6],
   }, {
     userID: 3,
     username: 'oliverzheng',
     firstName: 'Oliver',
+    groups: [1, 4, 5],
   }],
+
+  groups: [{
+    groupID: 1,
+    name: 'Seattle',
+  }, {
+    groupID: 2,
+    name: 'Non-Seattle',
+  }, {
+    groupID: 3,
+    name: 'Microsoft',
+  }, {
+    groupID: 4,
+    name: 'Silicon Valley Companies',
+  }, {
+    groupID: 5,
+    name: 'Macbook Pro Owners',
+  }, {
+    groupID: 6,
+    name: 'Macbook Air Owners',
+  }],
+
+  groupCompetitions: [{
+    group1ID: 1,
+    group2ID: 2,
+  }, {
+    group1ID: 3,
+    group2ID: 4,
+  }, {
+    group1ID: 5,
+    group2ID: 6,
+  }],
+
   poops: [{
     userID: 1,
     timestamp: 1517623604,
