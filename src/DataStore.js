60198f82b5b330d51c667bf26919d84cac52a6a4
Oliver Zheng <gitcommits@oliverzheng.com>
2018-02-04 17:07:48 -0800

    Add stub data

---
+++ src/DataStore.js
@@ -0,0 +1,27 @@
+const DataStore = {
+  users: [{
+    userID: 1,
+    username: 'joshcarrier',
+    firstName: 'Josh',
+  }, {
+    userID: 2,
+    username: 'qijinzhou',
+    firstName: 'Qijin',
+  }, {
+    userID: 3,
+    username: 'oliverzheng',
+    firstName: 'Oliver',
+  }],
+  poops: [{
+    userID: 1,
+    timestamp: 1517623604,
+  }, {
+    userID: 2,
+    timestamp: 1517603604,
+  }, {
+    userID: 3,
+    timestamp: 1517611000,
+  }],
+};
+
+export default DataStore;
