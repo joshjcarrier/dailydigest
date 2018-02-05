33f75abf2c2f682e9d1f73f0a639180ccf3e16ac
Josh Carrier <joshjcarrier@gmail.com>
2018-02-04 19:49:16 -0800

    feat(tabs): Tab items work

---
+++ src/LoginView.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import {
+  FlatList, 
+  Text,
+  View, 
+} from 'react-native';
+
+export default class LoginView extends React.Component {
+  render() {
+    return (
+      <View>
+        <Text>Hello</Text>
+        {/* <FlatList>
+          <Text>Hello</Text>
+        </FlatList> */}
+      </View>
+    );
+  }
+}
--- src/RootView.js
+++ src/RootView.js
@@ -12,9 +12,10 @@ import {
   View, 
 } from 'react-native';
 import MapView from 'react-native-maps';
-import { LineChart } from 'react-native-svg-charts'
-import { graphql } from 'react-apollo'
-import gql from 'graphql-tag'
+import { LineChart } from 'react-native-svg-charts';
+import { graphql } from 'react-apollo';
+import gql from 'graphql-tag';
+import LoginView from './LoginView';
 
 const allUsersQuery = gql`
   query {
@@ -81,61 +82,62 @@ class RootView extends React.Component {
   render() {
     return (
       <View style={{ flex: 1}}>
-        <Modal
-          transparent={true}
-          visible={this.state.poopCompleteVisible}
-          animationType={'fade'}
-          onRequestClose={() => this._onPoopSummaryClose()} >
-          <View style={{ flex: 1, margin: 32, backgroundColor: 'white' }}>
-            <View style={{ padding: 16 }}>
-              <Button
-                  onPress={() => this._onPoopSummaryClose()}
-                  title="Close modal">
-              </Button>
-              <Text style={{ fontSize: 54 }}>{(this.state.poopSize._value / this.POOP_MAXIMUM_FONT_SIZE)*100}%</Text>              
-            </View>
-          </View>
-        </Modal>
-        <View style={{ flex: 4, backgroundColor: 'red' }}>
-          <MapView initialRegion={{
-            latitude: 47.620440,
-            longitude: -122.347173,
-            latitudeDelta: 0.001,
-            longitudeDelta: 0.001
-          }} style={{ flex: 1}}/>
-        </View>
-        <View style={{ flex: 8 }}>
-          <FlatList
-            data={[
-              {key: JSON.stringify(this.state.users), stats: [1, 2, 3, 4, 5]},
-              {key: 'Facebook', stats: [1, 2, 3, 4, 5]},
-              {key: 'Google', stats: [5, 3, 1, 3, 5]},
-              {key: 'UBC', stats: [5, 4, 3, 2, 1]},
-            ]}
-            renderItem={({item}) => {
-              return (
-                <View style={{ borderBottomWidth: 1, borderBottomColor: 'lightgrey' }}>
-                  <Text style={{ textAlign: 'center', padding: 32 }}>
-                    {item.key}
-                  </Text>
-                  <LineChart dataPoints={ item.stats } style={{ height: 50 }} svg={{ stroke: '#795548' }} showGrid={ false }/>
+        <TabBarIOS>
+          <TabBarIOS.Item title='Home' systemIcon='featured' selected={this._isTabSelected('home')} onPress={() => {this._onPressTab('home');}}>
+            <View style={{ flex: 1 }}>
+              <Modal
+                transparent={true}
+                visible={this.state.poopCompleteVisible}
+                animationType={'fade'}
+                onRequestClose={() => this._onPoopSummaryClose()} >
+                <View style={{ flex: 1, margin: 32, backgroundColor: 'white' }}>
+                  <View style={{ padding: 16 }}>
+                    <Button
+                        onPress={() => this._onPoopSummaryClose()}
+                        title="Close modal">
+                    </Button>
+                    <Text style={{ fontSize: 54 }}>{(this.state.poopSize._value / this.POOP_MAXIMUM_FONT_SIZE)*100}%</Text>              
+                  </View>
                 </View>
-              );
-             }} />
-        </View>
-        <View style={{ flex: 2, padding: 16, alignContent: 'center', justifyContent: 'center' }}>
-            <TouchableOpacity style={{ alignItems: 'center' }} hitSlop={{ left: 8, top: 8, right: 8, bottom: 8 }} onPressIn={() => this._onPoopIn()} onPressOut={() => this._onPoopOut()}>
-              <Animated.Text style={{ fontSize: this.state.poopSize }}>💩</Animated.Text>
-            </TouchableOpacity>
-        </View>
-        <View style={{ flex: 1}}>
-          <TabBarIOS>
-            <TabBarIOS.Item title='Home' systemIcon='featured' selected={this._isTabSelected('home')} onPress={() => {this._onPressTab('home');}}><View></View></TabBarIOS.Item>
-            <TabBarIOS.Item title='Me' systemIcon='contacts' selected={this._isTabSelected('me')} onPress={() => {this._onPressTab('me');}}><View></View></TabBarIOS.Item>
-            <TabBarIOS.Item title='Groups' systemIcon='favorites' badge={1} selected={this._isTabSelected('groups')} onPress={() => {this._onPressTab('groups');}}><View></View></TabBarIOS.Item>
-            <TabBarIOS.Item title='Settings' systemIcon='more' selected={this._isTabSelected('settings')} onPress={() => {this._onPressTab('settings');}}><View></View></TabBarIOS.Item>
-          </TabBarIOS>
-        </View>
+              </Modal>
+              <View style={{ flex: 4, backgroundColor: 'red' }}>
+                <MapView initialRegion={{
+                  latitude: 47.620440,
+                  longitude: -122.347173,
+                  latitudeDelta: 0.001,
+                  longitudeDelta: 0.001
+                }} style={{ flex: 1}}/>
+              </View>
+              <View style={{ flex: 8 }}>
+                <FlatList
+                  data={[
+                    {key: JSON.stringify(this.state.users), stats: [1, 2, 3, 4, 5]},
+                    {key: 'Facebook', stats: [1, 2, 3, 4, 5]},
+                    {key: 'Google', stats: [5, 3, 1, 3, 5]},
+                    {key: 'UBC', stats: [5, 4, 3, 2, 1]},
+                  ]}
+                  renderItem={({item}) => {
+                    return (
+                      <View style={{ borderBottomWidth: 1, borderBottomColor: 'lightgrey' }}>
+                        <Text style={{ textAlign: 'center', padding: 32 }}>
+                          {item.key}
+                        </Text>
+                        <LineChart dataPoints={ item.stats } style={{ height: 50 }} svg={{ stroke: '#795548' }} showGrid={ false }/>
+                      </View>
+                    );
+                  }} />
+              </View>
+              <View style={{ flex: 2, padding: 16, alignContent: 'center', justifyContent: 'center' }}>
+                  <TouchableOpacity style={{ alignItems: 'center' }} hitSlop={{ left: 8, top: 8, right: 8, bottom: 8 }} onPressIn={() => this._onPoopIn()} onPressOut={() => this._onPoopOut()}>
+                    <Animated.Text style={{ fontSize: this.state.poopSize }}>💩</Animated.Text>
+                  </TouchableOpacity>
+              </View>
+            </View>
+          </TabBarIOS.Item>
+          <TabBarIOS.Item title='Me' systemIcon='contacts' selected={this._isTabSelected('me')} onPress={() => {this._onPressTab('me');}}><View></View></TabBarIOS.Item>
+          <TabBarIOS.Item title='Groups' systemIcon='favorites' badge={1} selected={this._isTabSelected('groups')} onPress={() => {this._onPressTab('groups');}}><View></View></TabBarIOS.Item>
+          <TabBarIOS.Item title='Settings' systemIcon='more' selected={this._isTabSelected('settings')} onPress={() => {this._onPressTab('settings');}}><LoginView/></TabBarIOS.Item>
+        </TabBarIOS>
       </View>
     );
   }
