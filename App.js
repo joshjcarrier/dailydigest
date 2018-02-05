08c2bae2142659b481a7de4c30e9ba4b94fcecd2
Josh Carrier <joshjcarrier@gmail.com>
2018-02-04 17:29:28 -0800

    feat(maps): Maps rendering of locatio

--- App.js
+++ App.js
@@ -1,12 +1,18 @@
 import React from 'react';
 import { StyleSheet, Text, View, Button } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class App extends React.Component {
   render() {
     return (
       <View style={{ flex: 1}}>
         <View style={{ flex: 4, backgroundColor: 'red' }}>
-          <Text>Background map thing</Text>
+          <MapView initialRegion={{
+            latitude: 47.620440,
+            longitude: -122.347173,
+            latitudeDelta: 0.001,
+            longitudeDelta: 0.001
+          }} style={{ flex: 1}}/>
         </View>
         <View style={{ flex: 8, backgroundColor: 'blue' }}>
           <Text>Live tracking goes here</Text>
